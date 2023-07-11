4305d528a5e3329aa9fc5af33aad4bf622adf56a
Deepanshu Kamboj <78980433+deepkamboj07@users.noreply.github.com>
2023-07-12 01:33:58 +0530

    first committ

--- Routes/submisionRouter.js
+++ Routes/submisionRouter.js
@@ -20,7 +20,7 @@ router.post('/code',(req,res,next)=>{
         switch(language)
         {
             case 'cpp':{
-                var envData = { OS : "windows" , cmd : "g++",options:{timeout:20000}};
+                var envData = {OS : "linux" , cmd : "gcc",options:{timeout:20000}};
                 compiler.compileCPP(envData,code,(data)=>{
                     if(data.error)
                     {
@@ -40,7 +40,7 @@ router.post('/code',(req,res,next)=>{
                 break;
             }
             case 'java':{
-                var envData={OS:"windows", cmd:"java",options:{timeout:20000}};
+                var envData={OS:"linux", cmd:"java",options:{timeout:20000}};
                 compiler.compileJava(envData,code,(data)=>{
                     if(data.error)
                     {
@@ -111,7 +111,7 @@ router.post('/code',(req,res,next)=>{
                 break;
             }
             case 'java':{
-                var envData={OS:"windows", cmd:"java",options:{timeout:20000}};
+                var envData={OS:"linux", cmd:"java",options:{timeout:20000}};
                 compiler.compileJavaWithInput(envData,code,input,(data)=>{
                     if(data.error)
                     {
